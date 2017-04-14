84bf04f2bf97cff9570c754bb34b6ac69e8e66ab
KaplapureL <KaplapureL@NPF0JVX25.sqs.group.intl>
2017-04-14 12:08:35 +0530

    test

---
+++ src/main/java/GitHub_Test/GitHubTest1/App.java
@@ -0,0 +1,13 @@
+package GitHub_Test.GitHubTest1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/GitHub_Test/GitHubTest1/AppTest.java
@@ -0,0 +1,25 @@
+package GitHub_Test.GitHubTest1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AppTest {
+   
+    public void testApp() throws InterruptedException{    
+        System.setProperty("webdriver.chrome.driver","C://Selenium/chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        String eTitle = "Meet Guru99";
+        String aTitle = "";
+        driver.get("http://www.guru99.com");
+        driver.manage().window().maximize();
+        
+        aTitle = driver.getTitle();
+        
+        if (aTitle.contentEquals(eTitle)){
+        	System.out.println("Test Passed");
+        }else{
+        	System.out.println("Test Failed");	
+        }
+       driver.close();
+    }
+}
